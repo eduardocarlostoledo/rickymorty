57b8edc18b8a282f18ae82c2c8cfb830b7714829
eduardocarlostoledo <eduardocarlostoledo@gmail.com>
2023-10-11 17:16:54 -0300

    pushing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route } from "react-router-dom";
+import Home from "./components/Home";
+import Form from "./components/Form";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/form" component={Form} />
     </div>
   );
 }
 
 export default App;
+
+// COMPONENTE HOME
+// CARDS
+// CARD
+// FORM
+// NAVBAR
+
+// REDUX
+// ACTIONS
+// REDUCER
+// STORE
+
+// CONECTAR REACT CON REDUX => PROVIDER
+// DARLE A LA APP LA CAPACIDAD DE DEFINIR RUTAR => BROWSER ROUTER
